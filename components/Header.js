829427c8d3f02fa431870b048f910d3e1795c24e
Brian <trangb@oregonstate.edu>
2019-10-14 13:27:51 -0700

    yellow link text, wood background

--- components/Header.js
+++ components/Header.js
@@ -4,38 +4,65 @@ import styled from "styled-components";
 const StyledHeader = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: center;
-  padding-top: 1em;
-  margin-bottom:calc(5vmin);
+  padding-top: calc(2vmin);
+  justify-content:space-between;
+  background: ${props => props.backgroundImage};
+  background-position: 0 20%;
+  background-size: cover;
+  background-repeat: no-repeat;
 `;
 
 const StyledLogo = styled.img`
   width: 120px;
   height: 120px;
+  margin: 0 calc(5vmin + 10px);
 `;
 
-const StyledLinkWrapper = styled.div`
-  padding-top: 4em;
-  text-align: center;
+const LinkContainer = styled.span`
+  display: flex;
+  flex-direction: row;
+  padding: 0 calc(5vmin);
+  justify-content: flex-end;
+
 `;
-const linkStyle = {
-  color: "#76B251",
-  fontWeight:"800"
-};
+
+const StyledLinkWrapper = styled.span`
+  padding: calc(5vmin) ;
+  font-size:3rem;
+`;
+
+const AnchorStyled=styled.a`
+  color: #FCEE21;
+  font-weight:800;
+  text-decoration: none;
+  cursor:pointer;
+
+  :hover{
+    color:pink;
+  }
+`
 
 const Header = () => (
-  <StyledHeader>
-    <StyledLinkWrapper>
-      <Link href="/">
-        <a style={linkStyle}>home</a>
-      </Link>
-    </StyledLinkWrapper>
-    <StyledLogo src={"/static/logo.jpg"} />
-    <StyledLinkWrapper>
-      <Link href="/about">
-        <a style={linkStyle}>about</a>
-      </Link>
-    </StyledLinkWrapper>
+  <StyledHeader backgroundImage={`url(${"/static/hardwood.jpg"})`}>
+    <span><StyledLogo src={"/static/logo-trans.png"} /></span>
+
+    <LinkContainer className="NavLinks">
+      <StyledLinkWrapper>
+        <Link href="/">
+          <AnchorStyled >home</AnchorStyled>
+        </Link>
+      </StyledLinkWrapper>
+      <StyledLinkWrapper>
+        <Link href="/about">
+          <AnchorStyled >about</AnchorStyled>
+        </Link>
+      </StyledLinkWrapper>
+      <StyledLinkWrapper>
+        <Link href="/contact">
+          <AnchorStyled >contact</AnchorStyled>
+        </Link>
+      </StyledLinkWrapper>
+    </LinkContainer>
   </StyledHeader>
 );
 
